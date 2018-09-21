8c1dc9731b64077f8f59d79073e44881a074debc
Hozefa Mangalorewala <hozefam@gmail.com>
2018-09-22 02:32:29 +0300

    Added React Router and Babel

---
+++ src/Details.js
@@ -0,0 +1,64 @@
+import React from "react";
+import pf from "petfinder-client";
+import { navigate } from "@reach/router";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
+class Details extends React.Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    petfinder.pet
+      .get({ output: "full", id: this.props.id })
+      .then(data => {
+        const pet = data.petfinder.pet;
+
+        let breed;
+        if (Array.isArray(data.petfinder.pet.breeds.breed)) {
+          breed = data.petfinder.pet.breeds.breed.join(", ");
+        } else {
+          breed = data.petfinder.pet.breeds.breed;
+        }
+
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      .catch(err => {
+        navigate("/");
+      });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading ...</h1>;
+    }
+
+    const { name, animal, breed, location, description } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Details;
